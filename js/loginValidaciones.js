e8b7c0a3c4b08a1219762bae394ece86c4d73a85
Sofia Britos <sofiibritos01@gmail.com>
2022-09-22 20:46:59 -0300

    Orginanzando codigo y configurando para hacer crud

---
+++ js/loginValidaciones.js
@@ -0,0 +1,29 @@
+
+// const inputs = document.querySelectorAll("[data-input]");
+
+// inputs.forEach((input) =>{
+//     console.log(input);
+//     input.addEventListener("blur", ()=>{
+
+//     })
+// })
+
+// const inputPass = document.querySelector("[data-inputPassword]");
+
+// inputEmail.addEventListener("blur", ()=>{
+//     // console.log(inputEmail.target);
+//     if (inputEmail.target == null) {
+//         inputEmail.classList.add("input-container--invalid");
+//     }
+// });
+
+// // console.log(inputPass.target);
+// inputPass.addEventListener("blur", ()=>{
+//     // console.log(inputEmail.target);
+//     if (inputPass.target == null) {
+//         inputPass.classList.add("input-container--invalid");
+//     }
+// });
+
+
+// verificar existencia del usuario
